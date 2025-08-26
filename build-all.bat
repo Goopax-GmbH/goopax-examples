dff89d91df183e9481a02231b32363c004074c01
Fassio Blatter <fassio@specpose.com>
2025-08-26 07:20:57 +0200

    set all windows project targets to release

--- build-all.bat
+++ build-all.bat
@@ -6,13 +6,25 @@ set CMAKE_BUILD_PARALLEL_LEVEL=%NUMBER_OF_PROCESSORS%
 cmake %* %CMAKE_FLAGS% -DCMAKE_CONFIGURATION_TYPES="Debug;Release" -B build src
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-cmake --build build --target build_external_libraries
+cmake --build build --config Release --target build_boost
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+cmake --build build --config Release --target build_opencv
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+cmake --build build --config Release --target build_sdl3
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+cmake --build build --config Release --target build_eigen
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+cmake --build build --config Release --target build_glatter
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 cmake build
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-cmake --build build
+cmake --build build --config Release
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 endlocal
