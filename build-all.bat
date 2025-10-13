b358ed4e6df6a746aa2ecb2f11dabe793dedc68b
Fassio Blatter <fassio@specpose.com>
2025-10-13 20:46:48 +0200

    Bumped Windows OpenCV version

--- build-all.bat
+++ build-all.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 
 set CMAKE_BUILD_PARALLEL_LEVEL=%NUMBER_OF_PROCESSORS%
 
-cmake %* %CMAKE_FLAGS% -DCMAKE_CONFIGURATION_TYPES="Debug;Release" -B build src
+cmake %* %CMAKE_FLAGS% -B build src
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 cmake --build build --config Release --target build_external_libraries
