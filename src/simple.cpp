ec1c96959739558634101b604bded46156e92be2
Ingo Josopait <ingo.josopait@gmx.de>
2026-02-06 23:35:13 +0100

    reverting simple

--- src/simple.cpp
+++ src/simple.cpp
@@ -11,33 +11,11 @@ using namespace std;
 
 int main()
 {
-    goopax_device device = default_device(env_CUDA);
+    goopax_device device = default_device(env_ALL);
+    kernel foo(device, [](resource<int>& A) { gpu_for_global(0, A.size(), [&](gpu_uint i) { A[i] = i; }); });
 
-    const int width = 640;
-    const int height = 480;
+    buffer<int> A(device, 1000);
+    foo(A);
 
-#ifdef _WIN32
-    SYSTEM_INFO si;
-    GetSystemInfo(&si);
-    const size_t pagesize = si.dwPageSize;
-#else
-    const size_t pagesize = getpagesize();
-#endif
-
-    char* host_p =
-        static_cast<char*>(operator new[](width * height * sizeof(char), static_cast<std::align_val_t>(pagesize)));
-
-    buffer<char> goopax_buffer(device, width * height, host_p);
-
-    kernel get_sum(device, [&goopax_buffer]() {
-        gpu_uint sum = 0;
-        gpu_for_global(0, width * height, [&](gpu_uint k) { sum += goopax_buffer[k]; });
-        return gather_add(sum);
-    });
-
-    fill(host_p, host_p + width * height, 17);
-
-    cout << "average: " << get_sum().get() / (width * height) << endl;
-
-    operator delete[](host_p, static_cast<std::align_val_t>(pagesize));
+    cout << "A=" << A << endl;
 }
