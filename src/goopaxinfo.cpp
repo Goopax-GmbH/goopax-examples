9729e3b90f9b992d628efbc3aff0be2afbec420c
Ingo Josopait <josopait@goopax.com>
2026-02-21 22:22:48 +0100

    goopaxinfo

---
+++ src/goopaxinfo.cpp
@@ -0,0 +1,114 @@
+/**
+   \example goopaxinfo.cpp
+   Display goopax device information
+ */
+
+#include <goopax>
+#include <goopax_extra/output.hpp>
+
+using namespace goopax;
+using namespace std;
+
+static constexpr const char* backend_names[] = { "CPU", "OpenCL", "CUDA", "Metal", "Vulkan" };
+
+namespace std
+{
+template<typename T, size_t N>
+std::ostream& operator<<(std::ostream& s, const std::array<T, N>& a)
+{
+    s << "(";
+    for (size_t k = 0; k < N; ++k)
+    {
+        if (k != 0)
+        {
+            s << ", ";
+        }
+        s << a[k];
+    }
+    s << ")";
+    return s;
+}
+
+std::ostream& operator<<(std::ostream& s, const std::type_info* ti)
+{
+    if (ti != nullptr)
+    {
+        s << goopax::pretty_typename(*ti);
+    }
+    else
+    {
+        s << "unknown type";
+    }
+    return s;
+}
+}
+
+int main()
+{
+    for (unsigned int log_env = 0; log_env < 10; ++log_env)
+    {
+        goopax::envmode env = static_cast<goopax::envmode>(1 << log_env);
+        vector<goopax_device> dvec = goopax::devices(env);
+        if (!dvec.empty())
+        {
+            cout << "\nbackend: " << env;
+            if (log_env < std::size(backend_names))
+            {
+                cout << " (" << backend_names[log_env] << ")";
+            }
+            cout << endl;
+            for (size_t device_i = 0; device_i < dvec.size(); ++device_i)
+            {
+                auto& d = dvec[device_i];
+                cout << "  device " << device_i << "/" << dvec.size() << ":" << endl
+                     << "    name=" << d.name() << endl
+                     << "    vendor=" << d.vendor() << endl
+                     << "    local_size: default=" << d.default_local_size() << ", max=" << d.max_local_size() << endl
+                     << "    default global_size: " << d.default_global_size_min() << ".."
+                     << d.default_global_size_max() << endl
+                     << "    global memory: " << d.global_memory_total() << endl
+                     << "    cache_line=" << d.cache_line() << endl
+                     << "    cache_size=" << d.cache_size() << endl
+                     << "    registers: " << d.max_registers() << endl;
+                cout << "    supported types:";
+                const char* delim = " ";
+                for (auto* t : goopax::detail::type_enum_type_tab)
+                {
+                    if (d.support_type(*t))
+                    {
+                        cout << delim << goopax::pretty_typename(*t);
+                        delim = ", ";
+                    }
+                }
+                cout << endl;
+
+                cout << "    warp_matrix modes:" << endl;
+
+                const matrix_support_info* modes;
+                unsigned int N = detail::goopax_6_get_matrix_support_table(d.get_impl(), &modes);
+
+                if (N != 0)
+                {
+                    for (size_t k = 0; k < N; ++k)
+                    {
+                        cout << "      mode " << k << ": sizes=" << modes[k].sizes << ", ab=" << modes[k].type_enum_ab
+                             << ", c=" << modes[k].type_enum_c
+                             << ", type_AB=" << goopax::detail::get_reverse_type_enum(modes[k].type_enum_ab)
+                             << ", type_C=" << goopax::detail::get_reverse_type_enum(modes[k].type_enum_c) << endl;
+                    }
+                }
+                else
+                {
+                    if (static_cast<goopax::envmode>(env) == env_CPU)
+                    {
+                        cout << "      any" << endl;
+                    }
+                    else
+                    {
+                        cout << "      none" << endl;
+                    }
+                }
+            }
+        }
+    }
+}
