b20c2686b64c26752ef08c84e465705ba16fcee3
Ingo Josopait <josopait@goopax.com>
2026-05-27 14:22:22 +0200

    zero.hpp

--- src/cosmology/zero.hpp
+++ src/cosmology/zero.hpp
@@ -1,59 +1,27 @@
 #pragma once
+#include <goopax>
 
-template<typename T>
-struct has_zero : false_type
-{
-};
-
-template<typename T>
-    requires(std::is_convertible_v<decltype(T::Zero()), T>)
-struct has_zero<T> : true_type
-{
-};
-
-template<typename T>
-    requires(std::is_convertible_v<decltype(T::zero()), T>)
-struct has_zero<T> : true_type
-{
-};
-
-struct zero_initializer;
+#if __cpp_concepts >= 201907
 
 struct zero_initializer
 {
-    template<typename T>
-        requires(std::is_convertible_v<int, T> /*&& !(goopax::is_gpu_type<T>::value)*/
-                 && (is_same_v<typename goopax::get_gpu_mode<T>::type, int>
-                     || is_same_v<typename goopax::get_gpu_mode<T>::type, goopax::debugtype<int>>
-                     || is_same_v<typename goopax::get_gpu_mode<T>::type, goopax::gpu_int>))
-    operator T() const
+    operator int() const
     {
-        return static_cast<T>(0);
+        return 0;
+    }
+    template<typename T>
+    operator goopax::gpu_type<T>() const
+    {
+        return 0;
     }
 
     template<typename T>
-#if defined(__clang__)
-        requires(!std::is_convertible_v<int, T> && std::is_convertible_v<decltype(T::Constant({})), T>)
-#else
-        requires(!std::is_convertible_v<int, T> && std::is_convertible_v<decltype(T::Constant(zero_initializer())), T>)
-#endif
-    operator T() const
-    {
-        return T::Constant(zero_initializer());
-    }
-
-    template<typename T>
-        requires(!std::is_convertible_v<int, T> && std::is_convertible_v<decltype(T::zero()), T>)
-    operator T() const
-    {
-        return T::zero();
-    }
-
-    template<typename T>
-        requires(!has_zero<T>::value) && (!std::is_convertible_v<int, T>)/*&& (!goopax::is_gpu_type<T>::value)*/
+        requires(!std::is_convertible_v<int, T>)
     operator T() const;
 };
 
+namespace zero_checks
+{
 template<typename T>
 struct has_fill : false_type
 {
@@ -66,19 +34,56 @@ struct has_fill<T> : true_type
 };
 
 template<typename T>
-    requires(!has_zero<T>::value) && (!std::is_convertible_v<int, T>)/*&& (!goopax::is_gpu_type<T>::value)*/
+struct has_Constant : false_type
+{
+};
+
+template<typename T>
+    requires(std::is_convertible_v<decltype(T::Constant(zero_initializer())), T>)
+struct has_Constant<T> : true_type
+{
+};
+
+template<typename T>
+struct has_zero : false_type
+{
+};
+
+template<typename T>
+    requires(std::is_convertible_v<decltype(T::zero()), T>)
+struct has_zero<T> : true_type
+{
+};
+}
+
+template<typename T>
+    requires(!std::is_convertible_v<int, T>)
 zero_initializer::operator T() const
 {
-    T ret;
-    if constexpr (has_fill<T>::value)
+    if constexpr (std::is_convertible<int, T>::value)
     {
-        ret.fill(static_cast<typename T::value_type>(zero_initializer()));
+        return 0;
+    }
+    else if constexpr (zero_checks::has_Constant<T>::value)
+    {
+        return T::Constant(*this);
+    }
+    else if constexpr (zero_checks::has_zero<T>::value)
+    {
+        return T::zero();
+    }
+    else if constexpr (std::ranges::input_range<T>)
+    {
+        T ret;
+        std::fill(ret.begin(), ret.end(), *this);
+        return ret;
     }
     else
     {
-        fill(ret, static_cast<typename T::value_type>(zero_initializer()));
+        static_assert(false, "Cannot find appropriate method to set to zero.");
     }
-    return ret;
 }
 
 static constexpr zero_initializer zero;
+
+#endif
